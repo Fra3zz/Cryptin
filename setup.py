a1bef943200bc54b0c89f5467562ee228beaf6e3
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-03-04 11:55:14 -0600

    Added and corrected timestamping dependices. Corrected module init files.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1' 
-DESCRIPTION = 'Simple xor operation'
-LONG_DESCRIPTION = 'Simple xor opeartions with the creation of xor object.'
+DESCRIPTION = 'Simple crypto operations'
+LONG_DESCRIPTION = "Simple crypto package of things that I don' see in other common cryptography modules"
 
 # Setting up
 setup(
@@ -13,10 +13,10 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=[], # add any additional packages that 
+        install_requires=["asn1tools"], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
 
-        keywords=['python', 'xor'],
+        keywords=['python', 'cryptography', "crypto"],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Cryptography",
