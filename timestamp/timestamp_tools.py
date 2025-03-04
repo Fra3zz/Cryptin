fb666b49370ec78b8b851cec122812964a6a254b
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-03-04 11:43:30 -0600

    Added timestamping function for creating a propperly formated timestamp request.

---
+++ timestamp/timestamp_tools.py
@@ -0,0 +1,32 @@
+import asn1tools
+from base64 import b64encode
+from hashlib import sha256
+
+
+def make_timestamp_request(message:bytes):
+
+    # Step 1: Compile the ASN.1 schema blueprint
+    tsp = asn1tools.compile_files('./tsp.asn')
+
+    # Step 2: Define the data to be encoded
+    data = {
+        'version': 1,
+        'messageImprint': {
+            'hashAlgorithm': {
+                'algorithm': '1.3.14.3.2.26'  # OID for SHA-1 256
+                # 'parameters': None  # Remove this line if parameters are not needed
+            },
+            'hashedMessage': sha256(message).digest() # Example hash value
+        },
+        #'reqPolicy': '1.3.6.1.4.1.12345.67.89.1',  # Example policy OID wors without it
+        'nonce': 1234567890,
+        'certReq': True,
+        'extensions': []  # Use an empty list for optional extensions
+    }
+
+    return tsp.encode('TimeStampReq', data)
+
+#Example usage
+# with open("request.der", "wb") as f:
+#     f.write(make_timestamp_request(b"Test"))
+#     f.close()
