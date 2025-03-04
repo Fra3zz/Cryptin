049bf3bd1d83ae134580d0026e35fc569d6b4fb2
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-03-04 16:06:22 -0600

    Changed OID code from sha1 to sha256.

--- crypt/timestamp/timestamp_tools.py
+++ crypt/timestamp/timestamp_tools.py
@@ -3,9 +3,7 @@ from base64 import b64encode
 from hashlib import sha256
 import pathlib
 
-
-def make_timestamp_request(message:bytes):
-
+def make_timestamp_request(message: bytes):
     # Step 1: Compile the ASN.1 schema blueprint
     tsp = asn1tools.compile_files(str(pathlib.Path(__file__).parent / "tsp.asn"))
 
@@ -14,12 +12,11 @@ def make_timestamp_request(message:bytes):
         'version': 1,
         'messageImprint': {
             'hashAlgorithm': {
-                'algorithm': '1.3.14.3.2.26'  # OID for SHA-1 256
-                # 'parameters': None  # Remove this line if parameters are not needed
+                'algorithm': '2.16.840.1.101.3.4.2.1',  # OID for SHA-256
+                #'parameters': None  # Remove this line if parameters are not needed
             },
-            'hashedMessage': sha256(message).digest() # Example hash value
+            'hashedMessage': sha256(message).digest()  # Example hash value
         },
-        #'reqPolicy': '1.3.6.1.4.1.12345.67.89.1',  # Example policy OID wors without it
         'nonce': 1234567890,
         'certReq': True,
         'extensions': []  # Use an empty list for optional extensions
@@ -27,7 +24,7 @@ def make_timestamp_request(message:bytes):
 
     return tsp.encode('TimeStampReq', data)
 
-#Example usage
+# Example usage
 # with open("request.der", "wb") as f:
 #     f.write(make_timestamp_request(b"Test"))
 #     f.close()
